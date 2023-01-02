c608299256043f5b6d5a75479fde9c8cb9160b2a
Marselino-Geradus <marselino914@gmail.com>
2023-01-02 21:40:27 +0800

    Day 78

---
+++ Latihan(6).py
@@ -0,0 +1,10 @@
+'''Ini adalah soal (2-Ronde 2) dalam lomba Coding War yang diselenggarakan oleh RPL Lab Unsulbar'''
+angka = int(input('Masukkan angka: '))
+if angka % 3 == 0:
+    print('Pride of 3')
+elif angka % 5 == 0:
+    print('Pride of 5')
+elif angka % 3 == 0 and angka % 5 == 0:
+    print('Master Class')
+else:
+    print('Bilangan tidak habis dibagi 3 atau 5')
