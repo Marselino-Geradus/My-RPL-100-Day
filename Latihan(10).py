41d1b52dba3a5600f15d7a05d59197f8c1596447
Marselino-Geradus <marselino914@gmail.com>
2023-01-06 07:07:31 +0800

    Day 82

---
+++ Latihan(10).py
@@ -0,0 +1,18 @@
+'''
+- Soal 3/Final Test
+- Coding War
+- RPL Lab Unsulbar
+'''
+
+#Proses Cari Jawaban 1
+
+#PREDIKSI KEUNTUNGAN INVESTASI
+modalAwal = int(input('Modal awal: '))
+lama_investasi = int(input('Lama investasi (tahun): '))
+
+keuntungan_tahun = 5/100 * modalAwal * lama_investasi
+modal2 = modalAwal + keuntungan_tahun
+
+tingkat_pertumbuhan_keuntungan_tiap_tahun = keuntungan_tahun * modal2
+print("Keuntungan yang diterima dalam setahun = ",keuntungan_tahun)
+print('Prediksi peningkatan keuntungan tiap tahun = ',tingkat_pertumbuhan_keuntungan_tiap_tahun)
