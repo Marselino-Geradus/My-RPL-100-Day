d30e22a0eca4019b8d7c34bc1eba3b37608091c8
Marselino-Geradus <marselino914@gmail.com>
2023-01-19 23:30:54 +0800

    Day 95

---
+++ Tabel Perkalian.py
@@ -0,0 +1,18 @@
+# Tabel perkalian dengan fungsi input
+print('='*32)
+kelipatan = int(input('> Tampilkan Tabel Perkalian: '))
+print('='*32)
+
+for a in range(1,11):
+    b = kelipatan*a
+    print(a,'x',kelipatan,'=',b)
+
+
+# # Tabel Pembagian dengan fungsi input
+# #Next_Commit baru dibahas
+# #Masih Mentah
+# print('~'*30)
+# bagi = int(input('Tampilkan Tabel Pembagian: '))
+# for a in range(11,1):
+#     a += 1
+#     print(bagi/a)
